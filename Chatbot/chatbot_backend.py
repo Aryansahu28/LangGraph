fe1e69a0f42e662c31e8281e2bd5a57f5d5a87c1
Aryansahu28 <aryansahu.vibranium@gmail.com>
2026-04-15 21:07:32 +0530

    Chatbot with streamlit and langgraph

---
+++ Chatbot/chatbot_backend.py
@@ -0,0 +1,41 @@
+from langgraph.graph import START,END,StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import BaseMessage
+from pydantic import BaseModel,Field
+from typing import Annotated,Optional,TypedDict
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto",  # let Hugging Face choose the best provider for you
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage],add_messages]
+
+def chatbot(state:ChatState):
+    message = state['messages']
+    result = chat_model.invoke(message)
+    return {'messages':[result]}
+
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chatbot',chatbot)
+
+graph.add_edge(START,'chatbot')
+graph.add_edge('chatbot',END)
+
+workflow = graph.compile(checkpointer=checkpointer)
